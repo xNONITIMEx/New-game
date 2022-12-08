471769c37401ad1df6a6b4044284a098e3d0bd0d
lдима <liahoffdim@yandex.tu>
2022-12-08 19:31:47 +0300

    Added separate player class

--- main.py
+++ main.py
@@ -1,87 +1,13 @@
 import pygame as pg
 from pygame.locals import *
+from Player_class import Player
 
 SIZE = WIDTH, HEIGHT = 400, 600
 FPS = 60
 
-GRAVITY = 0.5
-
 # Original velocity
 VELOCITY = 10
 
-
-# Player class
-class Player:
-    def __init__(self, player_image, position):
-        self.player_image = player_image
-        # Indicator of flipping image
-        self.flip = False
-        self.x, self.y = position
-        self.width, self.height = player_image.get_width(), player_image.get_height()
-        # Creating rectangle around the player
-        self.rect = self.player_image.get_rect()
-        self.rect.center = position
-
-        # Indicator of jumping process
-        self.is_jump = False
-        # Distance of moving up and down, kind of velocity
-        self.jump_offset = VELOCITY
-
-    # Drawing method
-    def draw(self, screen):
-        screen.blit(pg.transform.flip(self.player_image, self.flip, False), self.rect)
-        # Drawing rectangle around player
-        pg.draw.rect(screen, (0, 255, 0), self.rect, 2)
-
-    def move(self):
-        keys = pg.key.get_pressed()
-        # Delta x and delta y variables
-        dx = 0
-        dy = 0
-
-        # Process of jumping
-        # Info was taken from
-        # https://www.techwithtim.net/tutorials/game-development-with-python/pygame-tutorial/jumping/
-
-        # Check if player is not jumping
-        if self.is_jump is False:
-            # Switching jumping indicator to True
-            if keys[K_SPACE]:
-                self.is_jump = True
-        else:
-            # If velocity is lower than original velocity (VELOCITY)
-            if self.jump_offset >= -VELOCITY:
-                # If direction is positive, player goes up. If it's negative, player goes down
-                direction = 1
-                if self.jump_offset < 0:
-                    direction = -1
-                # Deducting amount of pixels, which is counting by multiplying quadratic velocity to
-                # coefficient which controls the amplitude of jump to
-                # direction
-                dy -= (self.jump_offset ** 2) * 0.4 * direction
-                # Reducing velocity
-                self.jump_offset -= 1
-            else:
-                self.jump_offset = VELOCITY
-                self.is_jump = False
-        # Moving by x-axis
-        if keys[K_d] and self.x <= WIDTH - self.width:
-            dx = 5
-            self.flip = False
-        if keys[K_a] and self.x >= 0:
-            dx = -5
-            self.flip = True
-        # Prohibiting player to move out of screen
-        if self.rect.left + dx <= 0:
-            dx = -self.rect.left
-        if self.rect.right + dx >= WIDTH:
-            dx = WIDTH - self.rect.right
-        if self.rect.top + dy <= 0:
-            dy = 10
-            self.is_jump = False
-        # Adding amount of pixels we will move to
-        self.rect.x += dx
-        self.rect.y += dy
 
 # Main function
 def main():
