48c1a820dcc10cf7219ca3fe6bf7baeb630d4810
lдима <liahoffdim@yandex.tu>
2023-02-06 19:29:18 +0300

    first task

---
+++ liceum_solutions2/main.py
@@ -0,0 +1,59 @@
+import os
+import sys
+
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+SCREEN_SIZE = [600, 450]
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.getImage()
+        self.initUI()
+
+    def getImage(self):
+        api_server = "http://static-maps.yandex.ru/1.x/"
+        lon = "37.530887"
+        lat = "55.703118"
+        delta = "0.002"
+        params = {
+            "ll": ",".join([lon, lat]),
+            "spn": ",".join([delta, delta]),
+            "l": "map"
+        }
+        response = requests.get(api_server, params=params)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        ## Изображение
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
