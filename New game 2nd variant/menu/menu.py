809df16edfdbffdb55ff896009a17190324a6c8e
Keeperofdark <115946474+Mirhful@users.noreply.github.com>
2023-02-27 18:47:19 +0300

    Add files via upload

--- New game 2nd variant/menu/menu.py
+++ New game 2nd variant/menu/menu.py
@@ -1,9 +1,10 @@
-import pygame
 import button
+import pygame
+import os
+import sys
 
 pygame.init()
 
-# create game window
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 800
 # pos fixed (in %) or free (in px)
@@ -12,33 +13,41 @@ position = "fixed"
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Main Menu")
 
-# game variables
 game_paused = 1
 menu_state = "main"
 
-# define fonts
 FONT_ARIAL_BLACK_30 = pygame.font.SysFont("arialblack", 30)
-FONT_INTER_BLACK_30 = pygame.font.Font("Inter-Light.ttf", 30)
+FONT_INTER_LIGHT_30 = pygame.font.Font("Inter-Light.ttf", 30)
+FONT_INTER_REGULAR_40 = pygame.font.Font("../resources/fonts/Inter/static/Inter-Regular.ttf", 40)
+FONT_INTER_REGULAR_50 = pygame.font.Font("../resources/fonts/Inter/static/Inter-Regular.ttf", 50)
 
-# define colours
 TEXT_COL = (255, 255, 255)
 
 # Инициализация списков для работы со слайдерами (sliders), кнопками, меняющимся текстом и т.д.
 display_greetings = ['Тык-тык', 'Тук-тук', 'Тач-тач', 'Клик-клик', 'Ту-ту-тук', 'Ты дятел?', 'Бесит да?']
-list_main_button_slider = ['Персонажи', 'Настройки', 'О разработке', 'Выйти']
+list_main_button_slider = ['Настройки', 'Персонажи', 'О разработке', 'Выйти']
+current_setting = list_main_button_slider[0]
 
 # load button images
 wallpaper1_img = pygame.image.load("img/wallpaper1.png").convert_alpha()
 arrow_left_img = pygame.image.load("img/btn_arrow_left.png").convert_alpha()
 arrow_right_img = pygame.image.load("img/btn_arrow_right.png").convert_alpha()
 options_img = pygame.image.load("img/horizontal_wide_button.png").convert_alpha()
+menu_options_img = pygame.image.load("img/menu_settings.png").convert_alpha()
+folder_img = pygame.image.load("img/btn_settings_folder.png").convert_alpha()
+volume_img = pygame.image.load("img/btn_settings_volume.png").convert_alpha()
+support_img = pygame.image.load("img/btn_settings_support.png").convert_alpha()
+level_1_img = pygame.image.load("img/btn_settings_level_1.png").convert_alpha()
+level_2_img = pygame.image.load("img/btn_settings_level_2.png").convert_alpha()
+level_3_img = pygame.image.load("img/btn_settings_level_3.png").convert_alpha()
+level_4_img = pygame.image.load("img/btn_settings_level_4.png").convert_alpha()
+
 quit_img = pygame.image.load("img/btn_options.png").convert_alpha()
 video_img = pygame.image.load('img/btn_options.png').convert_alpha()
 audio_img = pygame.image.load('img/btn_options.png').convert_alpha()
 keys_img = pygame.image.load('img/btn_options.png').convert_alpha()
 back_img = pygame.image.load('img/btn_options.png').convert_alpha()
 
-# create button instances
 if position == 'free':
     resume_button = button.Button(304, 125, options_img, 0.5)
     options_button = button.Button(297, 250, options_img, 0.5)
@@ -56,12 +65,15 @@ elif position == 'fixed':
                                0.75 * SCREEN_HEIGHT - options_img.get_height() // 3, arrow_left_img, 1)
     arrow_right = button.Button(SCREEN_WIDTH // 2 + options_img.get_width() // 2 + 0.35 * arrow_right_img.get_width(),
                                 0.75 * SCREEN_HEIGHT - options_img.get_height() // 3, arrow_right_img, 1)
-    resume_button = button.Button(SCREEN_WIDTH // 2 - options_img.get_width() // 2, 250, options_img, 1)
-    quit_button = button.Button(SCREEN_WIDTH // 2 - options_img.get_width() // 2, 375, quit_img, 1)
-    video_button = button.Button(SCREEN_WIDTH // 2 - options_img.get_width() // 2, 75, video_img, 1)
-    audio_button = button.Button(SCREEN_WIDTH // 2 - options_img.get_width() // 2, 200, audio_img, 1)
-    keys_button = button.Button(SCREEN_WIDTH // 2 - options_img.get_width() // 2, 325, keys_img, 1)
-    back_button = button.Button(SCREEN_WIDTH // 2 - options_img.get_width() // 2, 450, back_img, 1)
+    menu_options = button.Button(SCREEN_WIDTH // 2 - menu_options_img.get_width() // 2, SCREEN_HEIGHT * 0.325,
+                                 menu_options_img, 1)
+    btn_menu_settings_volume = button.Button(SCREEN_WIDTH * 0.22, SCREEN_HEIGHT * 0.46, volume_img, 1)
+    btn_menu_settings_folder = button.Button(SCREEN_WIDTH * 0.43, SCREEN_HEIGHT * 0.46, folder_img, 1)
+    btn_menu_settings_support = button.Button(SCREEN_WIDTH * 0.64, SCREEN_HEIGHT * 0.46, support_img, 1)
+    btn_menu_settings_level_1 = button.Button(SCREEN_WIDTH * 0.15, SCREEN_HEIGHT * 0.66, level_1_img, 1)
+    btn_menu_settings_level_2 = button.Button(SCREEN_WIDTH * 0.52, SCREEN_HEIGHT * 0.66, level_2_img, 1)
+    btn_menu_settings_level_3 = button.Button(SCREEN_WIDTH * 0.15, SCREEN_HEIGHT * 0.8, level_3_img, 1)
+    btn_menu_settings_level_4 = button.Button(SCREEN_WIDTH * 0.52, SCREEN_HEIGHT * 0.8, level_4_img, 1)
 
 
 def draw_text(text, font, text_col, x, y, pos="left"):
@@ -92,55 +104,71 @@ def change_slider_position(data, direction):
     return data[pos]
 
 
-# game loop
 run = True
 pos = 0
 while run:
 
     screen.blit(wallpaper1_img, wallpaper1_img.get_rect())
-    # check if game is paused
     if game_paused == 1:
         text1, text2 = "Нажмите пробел", "для продолжения"
-        # check menu state
         if menu_state == "main":
             options_button.draw(screen)
-            draw_text(list_main_button_slider[0], FONT_INTER_BLACK_30, (217, 102, 102),
-                      SCREEN_WIDTH // 2, 600, "center")
+            arrow_left.draw(screen)
+            arrow_right.draw(screen)
+            draw_text(current_setting, FONT_INTER_REGULAR_40,
+                      (217, 102, 102), SCREEN_WIDTH // 2, SCREEN_HEIGHT * 0.745, "center")
+            # draw_text(list_main_button_slider[0], FONT_INTER_REGULAR_40, (217, 102, 102),
+            #           SCREEN_WIDTH // 2, 594, "center")
             # text3 = FONT_INTER_BLACK_30.render("Помогите!", True, (217, 102, 102))
-
             if len(text1) * 5 < SCREEN_WIDTH // 2:  # Проверяю длину текста, чтобы он не выходил за границы
-                draw_text(text1, FONT_INTER_BLACK_30, TEXT_COL, SCREEN_WIDTH // 2, 200, "center")
-                draw_text(text2, FONT_INTER_BLACK_30, TEXT_COL, SCREEN_WIDTH // 2, 250, "center")
+                draw_text(text1, FONT_INTER_LIGHT_30, TEXT_COL, SCREEN_WIDTH // 2, 200, "center")
+                draw_text(text2, FONT_INTER_LIGHT_30, TEXT_COL, SCREEN_WIDTH // 2, 250, "center")
             # draw pause screen buttons
-            # if options_button.draw(screen):
-            #     menu_state = "options"
-            if arrow_left.draw(screen):
-                res = change_slider_position(list_main_button_slider, "previous")
-                print(res)
-                draw_text(res, FONT_INTER_BLACK_30,
-                          (217, 102, 102), 50, 450, "center")
+            if options_button.on_click():
+                menu_state = "options"
+            if arrow_left.on_click():
+                current_setting = change_slider_position(list_main_button_slider, "previous")
+                options_button.set_type(current_setting)
+                print(current_setting)
                 print("Next option")
-            if arrow_right.draw(screen):
-                print(change_slider_position(list_main_button_slider, "next"))
+            if arrow_right.on_click():
+                current_setting = change_slider_position(list_main_button_slider, "next")
+                options_button.set_type(current_setting)
+                print(current_setting)
+                # draw_text(current_setting, FONT_INTER_LIGHT_30,
+                #           (217, 102, 102), 50, 450, "center")
                 print("Previous option")
-        # check if the options menu is open
         if menu_state == "options":
-            # draw the different options buttons
-            if video_button.draw(screen):
-                print("Video Settings")
-            if audio_button.draw(screen):
-                print("Audio Settings")
-            if keys_button.draw(screen):
-                print("Change Key Bindings")
-            if back_button.draw(screen):
-                menu_state = "main"
+            if options_button.get_type() == "Настройки":
+                menu_options.draw(screen)
+                draw_text("Настройки", FONT_INTER_REGULAR_50,
+                          (246, 76, 76), SCREEN_WIDTH * 0.5, SCREEN_HEIGHT * 0.4, "center")
+                btn_menu_settings_volume.draw(screen)
+                if btn_menu_settings_volume.on_click():
+                    level_volume = 0
+                btn_menu_settings_folder.draw(screen)
+                if btn_menu_settings_folder.on_click():
+                    path = os.path.abspath('./')
+                    os.system(f'start {os.path.realpath(path)}')
+                    # path = "C:/Users"
+                    # path = os.path.realpath(path)
+                    # os.startfile(path)
+                btn_menu_settings_support.draw(screen)
+                draw_text("Уровни", FONT_INTER_REGULAR_50,
+                          (246, 76, 76), SCREEN_WIDTH * 0.5, SCREEN_HEIGHT * 0.6, "center")
+                btn_menu_settings_level_1.draw(screen)
+                btn_menu_settings_level_2.draw(screen)
+                btn_menu_settings_level_3.draw(screen)
+                btn_menu_settings_level_4.draw(screen)
+            elif options_button.get_type() == "Выйти":
+                sys.exit(0)
+
     else:
         text1, text2 = "Нажмите пробел", "для остановки"
         if len(text1) * 5 < SCREEN_WIDTH // 2:  # Проверяю длину текста, чтобы он не выходил за границы
-            draw_text(text1, FONT_INTER_BLACK_30, TEXT_COL, SCREEN_WIDTH // 2, 200, "center")
-            draw_text(text2, FONT_INTER_BLACK_30, TEXT_COL, SCREEN_WIDTH // 2, 250, "center")
+            draw_text(text1, FONT_INTER_LIGHT_30, TEXT_COL, SCREEN_WIDTH // 2, 200, "center")
+            draw_text(text2, FONT_INTER_LIGHT_30, TEXT_COL, SCREEN_WIDTH // 2, 250, "center")
 
-    # event handler
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
